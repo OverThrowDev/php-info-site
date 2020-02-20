cd2758ea2c8d944c3005dd4fabb2ffb25372bcc4
OverThrowDev <52225048+OverThrowDev@users.noreply.github.com>
2020-02-19 21:17:03 -0500

    Create 404.php

---
+++ 404.php
@@ -0,0 +1,30 @@
+
+
+<!DOCTYPE HTML>
+<html>
+ <head>
+   <title>404</title>
+   <script src="/cdn-cgi/apps/head/tmLLcnDXTZD7e1HZuR4adHT2jso.js"></script><script src="/cdn-cgi/apps/head/tmLLcnDXTZD7e1HZuR4adHT2jso.js"></script>
+   <link href="https://overthrowdev.com/css.css" rel="stylesheet" type="text/css">
+   <link href="https://overthrowdev.com/code.png" rel="icon" type="img/png">
+   <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate">
+   <meta http-equiv="Pragma" content="no-cache">
+   <meta http-equiv="Expires" content="0">
+  </head>
+  <div class="header">
+    <center><img src="https://overthrowdev.com/404.png" alt="404" /></center>
+ </div>
+        
+    <br>
+    
+    <div class="textHolder">
+     <h3>I can't seem to find that page!</h3>
+     Check to make sure you entered the correct URL.
+     
+     <div class="btn">
+        <a href="/">Home</a>
+     </div>
+  </div>
+      
+
+</html>
