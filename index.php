7248c4240be094d6b82b4a288d928bdd6d15a423
OverThrowDev <52225048+OverThrowDev@users.noreply.github.com>
2020-02-19 21:20:37 -0500

    Update index.php

--- index.php
+++ index.php
@@ -12,27 +12,11 @@
    <meta http-equiv="Expires" content="0">
   </head>
   <div class="header">
-    <center><h1>OverThrowDev</h1></center>
+    <center><h1>PHP</h1></center>
  </div>
     
    <div class="navbar">
-    <a href="/">Home</a>
-    <a href="/about">About</a>
-    <a href="/contact">Contact Me</a>
+    <a href="/phpinfo.php">INFO</a>
    </div>
-    
-    <br>
-    
-    <div class="textHolder">
-      <h3>OverThrow is just a random developer on the internet.</h3>
-      
-            <div class="btn">
-       <a href="/about">More</a>
-    </div>
-  </div>
-      
- <div class="footer">
-    Copyright 2020 OverThrow <div>Icons made by <a href="https://www.flaticon.com/authors/smashicons" title="Smashicons">Smashicons</a> from <a href="https://www.flaticon.com/" title="Flaticon">Flaticon</a></div>
- </div>
 
 </html>
