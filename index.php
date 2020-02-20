27d1cba6266c6be2655b4aeb2a30a06c15931b2c
OverThrowDev <52225048+OverThrowDev@users.noreply.github.com>
2020-02-19 21:15:25 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,38 @@
-<?php
-phpinfo();
-?>
+
+<!DOCTYPE HTML>
+<html>
+ <head>
+   <title>OverThrowDev</title>
+   <script src="/cdn-cgi/apps/head/tmLLcnDXTZD7e1HZuR4adHT2jso.js"></script>
+   <link href="https://overthrowdev.com/css.css" rel="stylesheet" type="text/css">
+   <link href="https://overthrowdev.com/code.png" rel="icon" type="img/png">
+   <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate">
+   <meta name="viewport" content="width=device-width, initial-scale=1">
+   <meta http-equiv="Pragma" content="no-cache">
+   <meta http-equiv="Expires" content="0">
+  </head>
+  <div class="header">
+    <center><h1>OverThrowDev</h1></center>
+ </div>
+    
+   <div class="navbar">
+    <a href="/">Home</a>
+    <a href="/about">About</a>
+    <a href="/contact">Contact Me</a>
+   </div>
+    
+    <br>
+    
+    <div class="textHolder">
+      <h3>OverThrow is just a random developer on the internet.</h3>
+      
+            <div class="btn">
+       <a href="/about">More</a>
+    </div>
+  </div>
+      
+ <div class="footer">
+    Copyright 2020 OverThrow <div>Icons made by <a href="https://www.flaticon.com/authors/smashicons" title="Smashicons">Smashicons</a> from <a href="https://www.flaticon.com/" title="Flaticon">Flaticon</a></div>
+ </div>
+
+</html>
